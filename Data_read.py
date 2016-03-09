3141075652186a701116f5e03d778a33ccff011c
Yaolong Nie <nieyaolong@gmail.com>
2016-03-09 19:34:44 +0800

    Read module

---
+++ Data_read.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+__author__ = 'zhiquan'
+
+import codecs
+
+input_file = codecs.open("model_build.py", 'rb', encoding='utf8')
+
+print input_file
+
+
+input_file.close()
---
+++ data/__init__.py
@@ -0,0 +1,3 @@
+__author__ = 'zhiquan'
+
+import Data_read
